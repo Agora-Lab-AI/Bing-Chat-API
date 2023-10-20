f8aec2dd57551ae4ae1d19f300f902cbbeb95d2a
Zack <zack@zackbradshaw.com>
2023-10-19 19:44:48 -0500

    feat Add env requirements and fix for wsl

--- main2.py
+++ main2.py
@@ -1,18 +1,12 @@
 import os
 import logging
-import datetime
+import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from undetected_chromedriver import Chrome, ChromeOptions
 from dotenv import load_dotenv
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.binary_location = "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe"  # replace with your path
-driver = webdriver.Chrome(options=options)
 load_dotenv()
 
 class BingChatAPI:
@@ -22,8 +16,7 @@ class BingChatAPI:
         self.options.add_argument("--headless")
         self.driver = Chrome(options=self.options)
         self.cookie_value = cookie_value
-
-        #hi
+        self.limit_counter = 0  # Counter to manage chat rate limits
 
     def initialize(self):
         cookie = {"name": "_U", "value": self.cookie_value}
@@ -32,37 +25,49 @@ class BingChatAPI:
         self.driver.refresh()
 
     def send_message(self, message: str):
-        chat_box = WebDriverWait(self.driver, 90).until(
-            EC.presence_of_element_located((By.ID, "searchbox"))  # Replace with the actual ID
-        )
-        chat_box.send_keys(message)
-        chat_box.send_keys(Keys.RETURN)
+        # Execute JavaScript to send a chat message
+        js_script = f"""
+        var chatBox = document.querySelector('your_chatbox_selector_here');
+        chatBox.value = '{message}';
+        chatBox.dispatchEvent(new Event('change'));
+        """
+        self.driver.execute_script(js_script)
+        self.limit_counter += 1  # Increment the counter
 
     def get_latest_message(self):
+        # Execute JavaScript to extract the latest chat message
+        js_script = """
+        var messages = document.querySelectorAll('your_message_selector_here');
+        return messages[messages.length - 1].textContent;
+        """
         try:
-            messages = WebDriverWait(self.driver, 60).until(
-                EC.presence_of_all_elements_located((By.CLASS_NAME, "response-message-group"))  # Replace with the actual class name
-            )
-            return messages[-1].text if messages else None
+            return self.driver.execute_script(js_script)
         except Exception as e:
             logging.critical(f"Error while extracting chat messages: {e}")
             return None
 
     def get_text_completion(self, initial_text: str):
-        self.initialize()
+        if self.limit_counter >= 5:  # Reset chat after 5 messages
+            self.initialize()
+            self.limit_counter = 0
+
         self.send_message(initial_text)
+        time.sleep(5)  # Wait for the chat service to respond; you can adjust this
         return self.get_latest_message()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    
+
     cookie_value = os.environ.get('BING_COOKIE', '')  # Read from environment variable
-    
+
     if not cookie_value:
         logging.error("BING_COOKIE environment variable not set. Exiting.")
         exit(1)
 
     bing_chat_api = BingChatAPI(cookie_value)
+    bing_chat_api.initialize()
+
     initial_text = "Once upon a time there was a cat named ______"
     completion = bing_chat_api.get_text_completion(initial_text)
+
     print(f"Initial text: {initial_text}\nCompletion: {completion}")
